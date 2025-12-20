a8bb8558fab759341a7e5eeac53927acef0548c7
rar4 <alexmish2009@gmail.com>
2025-12-20 19:15:55 +0100

    db and stuff

---
+++ DataManip.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using static System.Math;
+namespace bookrec.DataManip;
+
+
+
+internal class DataManip
+{
+    DataManip()
+    {
+        Console.WriteLine("IN data mainp");
+    }
+
+
+    public double CosineSimilarity(long book1, long book2)
+    {
+        int[] ratings1 = {};
+        int[] ratings2 = {};
+
+        double dot = 0; 
+        double magnitude1 = 0;
+        double magnitude2 = 0;
+
+        //TODO: Implement db interaction
+        //TODO: take 15 ratings from each book
+
+        for (int i = 0; i < 10; i++)
+        {
+            dot += ratings1[i] * ratings2[i];
+            magnitude1 += ratings1[i] * ratings1[i];
+            magnitude2 += ratings2[i] * ratings2[i];
+        }
+
+        magnitude1 = Math.Sqrt(magnitude1);
+        magnitude2 = Math.Sqrt(magnitude2);
+
+        return dot / (magnitude1 * magnitude2);
+
+
+
+        
+
+    }
+
+    
+}
---
+++ Models/Entities.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace bookrec.Models
+{
+    public class Book
+    {
+        public int Id;
+        [Required]
+        public int Isbn;
+    }
+
+    
+    public class Rating
+    {
+        public 
+    }
+}
+
+
